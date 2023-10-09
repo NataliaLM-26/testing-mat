bd55411b6cdb4f77208cdcee98957fb4fdf062e7
NataliaLM-26 <t.natalialm@gmail.com>
2023-10-08 20:46:16 -0600

    conclusión de pruebas de factorial #2

--- src/app/factorial/factorial.spec.ts
+++ src/app/factorial/factorial.spec.ts
@@ -1,17 +1,40 @@
 import { factorial } from './factorial';
 
 describe('factorial test suite', () => {
+    
+    it('should return 0 if input is negative numbers', () => {
+        const result = factorial(-1);
+        expect(result).toBe(0);
+    });
+
     it('should return 1 if input is 0', () => {
         const result = factorial(0);
         expect(result).toBe(1);
     });
 
-    it('should calculate the factorial correctly for positive numbers', () => {
+    it('should return 2 if input is 2', () => {
+        const result = factorial(2);
+        expect(result).toBe(2);
+    });
+    
+    it('should return 6 if input is 3', () => {
+        const result = factorial(3);
+        expect(result).toBe(6);
+    });
+
+    it('should return 24 if input is 4', () => {
+        const result = factorial(4);
+        expect(result).toBe(24);
+    });
+
+    it('should return 120 if input is 5', () => {
         const result = factorial(5);
         expect(result).toBe(120);
     });
 
-    it('should throw an error for negative numbers', () => {
-        expect(() => factorial(-1)).toThrow();
+    it('should return 0 if input > 15', () => {
+        const result = factorial(16);
+        expect(result).toBe(0);
     });
 });
+
--- src/app/factorial/factorial.ts
+++ src/app/factorial/factorial.ts
@@ -1,9 +1,17 @@
 function factorial(num: number): number {
-    if (num === 0) {
-        return 1;
+    if (num < 0) {
+        return 0;
     }
-    else {
-        return num * factorial(num - 1);
+    else if(num === 0){
+        return 1;
+    }else if(num>15){
+        return 0;
+    }else{
+        let result = 1;
+        for (let i=1;i<=num;i++){
+            result *= i;
+        }
+        return result;
     }
 }
 export {factorial};
