bcdc767557659e68426f7319e9ea1fb4ad3c8328
NataliaLM-26 <t.natalialm@gmail.com>
2023-11-16 22:28:13 -0600

    Avances en componente front3a #19

---
+++ src/app/front3a/front3a.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Front3aComponent } from './front3a.component';
+
+describe('Front3aComponent', () => {
+  let component: Front3aComponent;
+  let fixture: ComponentFixture<Front3aComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [Front3aComponent]
+    });
+    fixture = TestBed.createComponent(Front3aComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/front3a/front3a.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front3a',
+  templateUrl: './front3a.component.html',
+  styleUrls: ['./front3a.component.css']
+})
+export class Front3aComponent {
+
+}
