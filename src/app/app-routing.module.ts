7fb4070d088ff967bea787e41d7ac7b3557aca61
NataliaLM-26 <t.natalialm@gmail.com>
2023-12-04 09:01:32 -0600

    avances #28

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MainComponent } from './main/main.component';
 import { Front1aComponent } from './front1a/front1a.component';
 import { SimpsonComponent } from './simpson/simpson.component';
 import { Front3aComponent } from './front3a/front3a.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: '1a', component: Front1aComponent},
   {path: '3a', component: Front3aComponent},
-  {path: 'Simpson', component: SimpsonComponent}
+  {path: 'Simpson', component: SimpsonComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
